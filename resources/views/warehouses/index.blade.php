3e9d2f91f6cd5ee086a965ae60e4c28cdc1dd109
rakib <phi.rakib@gmail.com>
2024-07-11 18:38:00 +0600

    added session message component in warehouse index

--- resources/views/warehouses/index.blade.php
+++ resources/views/warehouses/index.blade.php
@@ -1,9 +1,5 @@
 <x-layout>
-    @if ($message = session('message'))
-        <div class="alert alert-success">
-            <p>{{ $message }}</p>
-        </div>
-    @endif
+    <x-session-message />
     
     <x-page-header title="Warehouse Index" routeName="warehouses.create" routeText="Create New Warehouse" />
     
