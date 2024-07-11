08d53595a3f30ff7fd5278fae174a54dc617574e
rakib <phi.rakib@gmail.com>
2024-07-11 18:39:33 +0600

    added product resource route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\ProductController;
 use App\Http\Controllers\WarehouseController;
 use Illuminate\Support\Facades\Route;
 
 Route::resource('warehouses', WarehouseController::class);
+Route::resource('products', ProductController::class);
